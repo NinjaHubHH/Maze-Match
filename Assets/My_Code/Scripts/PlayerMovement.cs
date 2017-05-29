5aeb41064674f87ba6360fb03880eeab202ef58f
NinjaHubHH <nruesch@live.de>
2017-05-29 22:05:59 +0200

    added little test scene, playerMovement Script

---
+++ Assets/My_Code/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+        speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime,speed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+
+    }
+}
