5e538782a08d49145388e6bb34ab5b0ffe90a59b
NinjaHubHH <nruesch@live.de>
2017-06-05 23:30:47 +0200

    minor changes, playermovement does not work the way it should yet

--- Assets/My_Code/Scripts/PlayerMovement.cs
+++ Assets/My_Code/Scripts/PlayerMovement.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed;
+    public float speed = 0.1f;
+    private Rigidbody2D player;
 
 	// Use this for initialization
 	void Start () {
 
-        speed = 2f;
+        player = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 
-        
-        transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime,speed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-Vector2.right * speed);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-Vector2.up * speed);
+        }
+
+
+
+
+        //float move = Input.GetAxis("Horizontal");
+
+        //player.velocity = new Vector2(move * speed, player.velocity.y);
+
+        // transform.Translate(speed * Input.GetAxis("Horizontal") * Time.deltaTime,speed * Input.GetAxis("Vertical") * Time.deltaTime, 0f);
 
     }
 }
