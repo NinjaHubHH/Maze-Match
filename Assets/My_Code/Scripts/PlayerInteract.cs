e67469eee5cca8004d8a475fb3ffc6a642575bb1
NinjaHubHH <nruesch@live.de>
2017-06-13 22:05:21 +0200

    startet with the picking up objects scripts -interactable Objects-

---
+++ Assets/My_Code/Scripts/PlayerInteract.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour {
+
+    public GameObject currentInteractableObj = null;
+
+    	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Function if you press button e to pick up a weapon/object
+        if (Input.GetButtonDown("Interact") && currentInteractableObj)
+        {
+            currentInteractableObj.SendMessage("DoInteraction");
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("interactableO"))
+        {
+            Debug.Log(other.name);
+            currentInteractableObj = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("interactableO"))
+        {
+            if (other.gameObject == currentInteractableObj)
+            {
+                currentInteractableObj = null;
+            }
+        }
+    }
+
+}
