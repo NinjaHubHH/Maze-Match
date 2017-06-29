067acb9c20ad62285ffe24022f7d23f6b4ec35af
NinjaHubHH <nruesch@live.de>
2017-06-29 17:25:43 +0200

    added menu scene with startGame implementation

---
+++ Assets/My_Code/Scripts/ButtonManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+    public void StartGameBtn(string gameLevel)
+    {
+        SceneManager.LoadScene(gameLevel);
+    }
+
+    public void ExitGameBtn()
+    {
+        Application.Quit();
+    }
+
+}
