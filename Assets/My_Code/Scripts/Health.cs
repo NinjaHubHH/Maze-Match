f5e4930550557dba337be7d42d142c4707313bf9
NinjaHubHH <nruesch@live.de>
2017-08-03 19:43:24 +0200

    minor changes, build in 1280 x 720 for the right 16:9 format

---
+++ Assets/My_Code/Scripts/Health.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    GameObject player;
+    bool playerInRange;
+
+    public int startingHealth = 6;
+    public int currentHealth;
+
+    public int attackDamage = 1;
+
+    public Sprite[] HeartSprites;
+    private Image HeartUI;
+
+    bool isDead;
+
+    // Use this for initialization
+    void Start () {
+        currentHealth = startingHealth;
+        HeartUI.sprite = HeartSprites[6];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (playerInRange)
+        {
+            Attack();
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+            Debug.Log("PlayerHealth" + " :" + currentHealth);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        Debug.Log(currentHealth);
+        HeartUI.sprite = HeartSprites[currentHealth];
+
+        //Player Death
+        if (currentHealth <= 0 && !isDead)
+        {
+            Die();
+        }
+
+    }
+
+    void Attack()
+    {
+        if (currentHealth > 0)
+        {
+            TakeDamage(attackDamage);
+        }
+    }
+    //if currenthealth = 0 the player dies 
+    void Die()
+    {
+        isDead = true;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
